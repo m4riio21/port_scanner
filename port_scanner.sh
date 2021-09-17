c42e720acc8290c0c7ab64c978075ac3df312d19
Mario González <78421275+m4riio21@users.noreply.github.com>
2021-09-17 20:18:57 +0200

    Args can be unordered now

--- port_scanner.sh
+++ port_scanner.sh
@@ -2,6 +2,17 @@
 
 # Inspired by S4vitaar (https://github.com/s4vitar)
 
+#Colours
+
+green="\e[0;32m\033[1m"
+end="\033[0m\e[0m"
+red="\e[0;31m\033[1m"
+blue="\e[0;34m\033[1m"
+yellow="\e[0;33m\033[1m"
+purple="\e[0;35m\033[1m"
+turquoise="\e[0;36m\033[1m"
+gray="\e[0;37m\033[1m"
+
 trap ctrl_c INT
 
 function ctrl_c() {
@@ -17,34 +28,38 @@ function helpPanel(){
 	echo -e "\t\t --full-scan: scans the full range of ports (1-65535). May take a while (3 - 6 min).\n"
 	echo -e "\t Usage:"
 	echo -e "\t\t $0 [scan type] {IP_TO_SCAN}"
+	tput cnorm
 }
 
 tput civis
+scan_type="none"
+ip="none"
+ports=""
+
+for arg in "$@"; do 	#Iterate args to search IP and scan type
+
+	if [[ $arg == "--fast-scan" || $arg == "--full-scan" && $scan_type == "none" ]]; then
+		scan_type=$arg
+	fi
+	if [[ $arg == "localhost" || arg=$(echo $arg | grep -oP '\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}') && $ip == "none" ]]; then
+		ip=$arg
+	fi
+done
+
 if [[ $# == "1" || $# == "2" ]]; then
-	scan_type=$1; ip=$2
-	code="none"
+	if [ $scan_type == "none" ]; then scan_type="--full-scan"; fi
 
-	timeout 1 bash -c "ping -c 1 $1" &>/dev/null
-	status=$?
-
-        if [[ "$status" == 0 ]]; then code="ok"; ip=$1; scan_type="--full-scan"; fi
-
-	timeout 1 bash -c "ping -c 1 $2" &>/dev/null
-	status=$?
-
-	if [[ "$status" -gt 0 && $code == "none" ]]; then echo "[*] Host unreachable"; tput cnorm; exit 1; fi
-	
-	if [ $scan_type == "--fast-scan" ]; then
-		echo -e "[*] Scanning top $(cat top_ports.txt | wc -l) ports\n"
-        	for port in $(cat top_ports.txt); do
-                	timeout 1 bash -c "echo > /dev/tcp/$ip/$port" 2>/dev/null && echo "[!] PORT $port - OPEN" &
+	if [ $scan_type == "--fast-scan" ]; then	# Fast scan
+		echo -e "[*] Scanning top $(wget -O- -q https://raw.githubusercontent.com/m4riio21/port_scanner/main/top_ports.txt | wc -l) ports\n"
+        	for port in $(wget -O- -q https://raw.githubusercontent.com/m4riio21/port_scanner/main/top_ports.txt); do
+                	timeout 1 bash -c "echo > /dev/tcp/$ip/$port" 2>/dev/null && echo -e "[!] PORT ${red}$port${end} - OPEN" &
         	done; wait
 		tput cnorm; exit 0
 	fi
-	if [ $scan_type == "--full-scan" ]; then
+	if [ $scan_type == "--full-scan" ]; then	# Full scan
         	echo -e "[*] Scanning full range of ports (1-65535)\n"
 	        for port in $(seq 1 65535); do
-        	        timeout 1 bash -c "echo > /dev/tcp/$ip/$port" 2>/dev/null && echo "[!] PORT $port - OPEN" &
+        	        timeout 1 bash -c "echo > /dev/tcp/$ip/$port" 2>/dev/null && echo -e "[!] PORT ${red}$port${end} - OPEN" &
 	        done; wait
 		tput cnorm; exit 0
 	fi
